450066be37501c7bd8a6150d213ed40bed85aaf5
pesparza <pesparza@PESPARZA-LAP.oradev.oraclecorp.com>
2017-01-29 23:25:13 -0600

    Test

---
+++ src/com/class16/Car.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.class16;
+
+/**
+ *
+ * @author pesparza
+ */
+public class Car {
+    private float accelaration = 9.16f;
+    private String carColor;
+    private boolean automatic;
+    
+    public void setCarColor(String color){
+        if (color.equalsIgnoreCase("YeLlOw")){
+          carColor = color;
+        }
+    }
+    
+    public boolean getTransmission(){
+        return automatic;
+    }
+    
+    public void setAccelaration(float accelaration){
+        this.accelaration = accelaration;
+    }
+    
+    public void printCarColor(){
+        System.out.println("My Color is: "+this.carColor);
+    }
+    
+    public void printAccelaration(){
+        System.out.println("This is the acceleration: "+this.accelaration);
+    }
+    
+}
---
+++ src/com/class16/DealerCar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.class16;
+
+/**
+ *
+ * @author pesparza
+ */
+public class DealerCar {
+    public static void main(String args[]){
+        Car bumbleBee = new Car();
+        bumbleBee.setCarColor("Red");
+        bumbleBee.printCarColor();
+        
+        bumbleBee.setCarColor("Yellow");
+        bumbleBee.printCarColor();
+        
+    }
+}
